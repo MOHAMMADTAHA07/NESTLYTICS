a2fe7d46a4290ea7976e9ad8e7abfc46209b9a2b
MOHAMMAD TAHA <mohdt7249@gmail.com>
2026-03-21 00:49:15 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/PosNeg.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PosNeg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num > 0){
+            System.out.println("Number is Positive");
+        }
+        else if (num < 0) {
+            System.out.println("Number is Negative");
+        }
+        else{
+            System.out.println("Number is Zero");
+        }
+    }
+}
