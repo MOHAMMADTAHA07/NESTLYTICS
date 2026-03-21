1ee632d6fb616f399b189921dc8de22706b693ef
MOHAMMAD TAHA <mohdt7249@gmail.com>
2026-03-22 01:05:11 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/PrimeRange.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class PrimeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter starting num ");
+        int start = sc.nextInt();
+        System.out.print("Enter ending num ");
+        int end = sc.nextInt();
+        for(int num = start; num <= end; num++) {
+            int count = 0;
+            for(int i = 1; i <= num; i++) {
+                if(num % i == 0) {
+                    count++;
+                }
+            }
+            if(count == 2) {
+                System.out.println(num);
+            }
+        }
+    }
+}
